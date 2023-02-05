2103601b1a42bab0486d4d76dad6a31501ffee9a
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-02-06 00:09:57 +0900

    first commit

---
+++ spring-authorization-server-tutorial/src/main/java/com/example/springauthorizationservertutorial/SpringAuthorizationServerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthorizationservertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationServerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationServerTutorialApplication.class, args);
+	}
+
+}
